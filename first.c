f9140150133fff461faf64e1d91c31653c640b8e
24-RishiRaj <satyarishi73@gmail.com>
2025-09-03 13:16:00 +0530

    Hello_World.c

--- first.c
+++ first.c
@@ -2,13 +2,7 @@
 
 int main()
 {
-    echo "# Git_Space" >> README.md
-                                  git init
-                                      git add README.md
-                                          git commit -
-                              m "first commit" git branch - M main git remote add origin https : // github.com/24-RishiRaj/Git_Space.git
-                                                                                                 git push -
-                                                                                                 u origin main
-                                                                                                     printf("Hello_World- this is my first 'C' programme");
+    printf("Hello_World- this is my first 'C' programme");
     return 0;
 }
+
